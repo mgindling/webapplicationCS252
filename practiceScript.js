7238189417fd85f8bf5aa875cb19850b6da5d0f9
Mark Gindling <mgindlin@purdue.edu>
2019-04-16 17:50:35 -0400

    Saturn is mean >:(

--- practiceScript.js
+++ practiceScript.js
@@ -2,33 +2,46 @@ function changePlanet(parameter1) {
     if (parameter1 == "mercury") {
         document.getElementById("planet").src = "Resources/mercury.png";
         document.getElementById("planet").height = 130;
+        document.getElementById("imgCol").style.marginTop = '0px';
+
     }
     else if (parameter1 == "venus") {
         document.getElementById("planet").src = "Resources/venus.png";
         document.getElementById("planet").height = 130;
+        document.getElementById("imgCol").style.marginTop = '0px';
+
     }
     else if (parameter1 == "earth") {
         document.getElementById("planet").src = "Resources/earth.png";
         document.getElementById("planet").height = 130;
+        document.getElementById("imgCol").style.marginTop = '0px';
+
     }
     else if (parameter1 == "mars") {
         document.getElementById("planet").src = "Resources/mars.png";
         document.getElementById("planet").height = 130;
+        document.getElementById("imgCol").style.marginTop = '0px';
+
     }
     else if (parameter1 == "jupiter") {
         document.getElementById("planet").src = "Resources/jupiter.png";
         document.getElementById("planet").height = 130;
+        document.getElementById("imgCol").style.marginTop = '0px';
+
     }
     else if (parameter1 == "saturn") {
         document.getElementById("planet").src = "Resources/saturn.png";
-        document.getElementById("planet").height = 80;
+        document.getElementById("planet").height = 70;
+        document.getElementById("imgCol").style.marginTop = '40px';
     }
     else if (parameter1 == "uranus") {
         document.getElementById("planet").src = "Resources/uranus.png";
         document.getElementById("planet").height = 130;
+        document.getElementById("imgCol").style.marginTop = '0px';
     }
     else if (parameter1 == "neptune") {
         document.getElementById("planet").src = "Resources/neptune.png";
         document.getElementById("planet").height = 130;
+        document.getElementById("imgCol").style.marginTop = '0px';
     }
 }
