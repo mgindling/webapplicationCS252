4ede5d267dfa086f99cebe1b88bd496cd3d10d6f
Mark Gindling <mgindlin@purdue.edu>
2019-04-16 17:41:58 -0400

    Added a javascript file; it changes the buttons :0

---
+++ practiceScript.js
@@ -0,0 +1,34 @@
+function changePlanet(parameter1) {
+    if (parameter1 == "mercury") {
+        document.getElementById("planet").src = "Resources/mercury.png";
+        document.getElementById("planet").height = 130;
+    }
+    else if (parameter1 == "venus") {
+        document.getElementById("planet").src = "Resources/venus.png";
+        document.getElementById("planet").height = 130;
+    }
+    else if (parameter1 == "earth") {
+        document.getElementById("planet").src = "Resources/earth.png";
+        document.getElementById("planet").height = 130;
+    }
+    else if (parameter1 == "mars") {
+        document.getElementById("planet").src = "Resources/mars.png";
+        document.getElementById("planet").height = 130;
+    }
+    else if (parameter1 == "jupiter") {
+        document.getElementById("planet").src = "Resources/jupiter.png";
+        document.getElementById("planet").height = 130;
+    }
+    else if (parameter1 == "saturn") {
+        document.getElementById("planet").src = "Resources/saturn.png";
+        document.getElementById("planet").height = 80;
+    }
+    else if (parameter1 == "uranus") {
+        document.getElementById("planet").src = "Resources/uranus.png";
+        document.getElementById("planet").height = 130;
+    }
+    else if (parameter1 == "neptune") {
+        document.getElementById("planet").src = "Resources/neptune.png";
+        document.getElementById("planet").height = 130;
+    }
+}
