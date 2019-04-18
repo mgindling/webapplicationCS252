1a14ba7ecf0a0d2057fb03ed99096b908dda7c2b
Mark Gindling <mgindlin@purdue.edu>
2019-04-18 12:59:43 -0400

    Added significant robustness to the Javascript
    Most of the math-based stuff, sans the time calculation (and the graph, of course), is done completely, at least for the practice "round". I kind of want to add that rgb-tinted Zorg planet or something.

--- practiceScript.js
+++ practiceScript.js
@@ -1,16 +1,49 @@
 var currentgravity = -9.8;
 
+/* Performs the main calculator function */
 function calculateArc() {
     var angle = document.getElementById("angle").value;
     var power = document.getElementById("power").value;
+    var isCorrect = false;
 
-    /*  -- Calculates the trajectory based on the maths of physics */
-    var trajectoryTime = (-2 * power) / (currentgravity);
-    var trajectoryDistance = (2 * power * Math.cos(angle * (Math.PI/180)));
+    /* Error checks the angle input box */
+    if (isNaN(angle)) {
+        document.getElementById("angle").value = "Angle not valid!";
+        isCorrect = true;
+    }
+    if (Number(angle) < 0) {
+        document.getElementById("angle").value = "Angle too small!";
+        isCorrect = true;
+    }
+    if (Number(angle) > 90) {
+        document.getElementById("angle").value = "Angle too large!";
+        isCorrect = true;
+    }
 
-    document.getElementById("shotInformation").innerHTML = "The projectile will stay in the air for " + trajectoryTime.toFixed(2) + " seconds and travel " + trajectoryDistance.toFixed(2) + " meters."
+    /* Error checks the power input box */
+    if (isNaN(power)) {
+        document.getElementById("power").value = "Power not valid!";
+        isCorrect = true;
+    }
+    if (Number(power) < 0) {
+        document.getElementById("power").value = "Power too small!";
+        isCorrect = true;
+    }
+
+    if (isCorrect == true) {
+        return;
+    }
+    else {
+
+        /* Calculates the trajectory based on the maths of physics */
+        var trajectoryTime = (-2 * power) / (currentgravity);
+        var trajectoryDistance = (2 * power * Math.cos(angle * (Math.PI / 180)));
+        document.getElementById("shotInformation").innerHTML = "The projectile will stay in the air for " + trajectoryTime.toFixed(2) + " seconds and travel " + trajectoryDistance.toFixed(2) + " meters."
+
+    }
 }
 
+/* Changes the picture of the planet on the practice calculator */
 function changePlanet(parameter1) {
     if (parameter1 == "mercury") {
         document.getElementById("planet").src = "Resources/mercury.png";
@@ -62,5 +95,22 @@ function changePlanet(parameter1) {
         document.getElementById("planet").height = 130;
         document.getElementById("imgCol").style.marginTop = '0px';
         currentgravity = -11.2;
+    }
+}
+
+/* Clears the text in the input box */
+function clearInput(parameter1) {
+    var name = parameter1;
+
+    if (name == "angle") {
+        if (isNaN(document.getElementById("angle").value)) { 
+            document.getElementById("angle").value = "";
+        }
+    }
+
+    if (name == "power") {
+        if (isNaN(document.getElementById("power").value)) {
+            document.getElementById("power").value = "";
+        }
     }
 }
