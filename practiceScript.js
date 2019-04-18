3eba155d6e42c211e5911bc09c09ac31d286c815
Mark Gindling <mgindlin@purdue.edu>
2019-04-18 19:33:39 -0400

    Added code from open source and cited it for the graph I'm not quite sure how it all works, though. Also, my math for the trajectory is completely wrong, in both time and distance.

--- practiceScript.js
+++ practiceScript.js
@@ -43,6 +43,41 @@ function calculateArc() {
     }
 }
 
+/* Draws the graph: Code Adapted from open source on https://mathjs.org/examples/browser/plot.html.html */
+function drawGraph() {
+    try {
+        // compile the expression once
+        const expression = document.getElementById('eq').value
+        const expr = math.compile(expression)
+
+        // evaluate the expression repeatedly for different values of x
+        const xValues = math.range(0, 50, 0.5).toArray()
+        const yValues = xValues.map(function (x) {
+            return expr.eval({ x: x })
+        })
+
+        // render the plot using plotly
+        const trace1 = {
+            x: xValues,
+            y: yValues,
+            type: 'scatter'
+        }
+        const data = [trace1]
+        Plotly.newPlot('plot', data)
+    }
+    catch (err) {
+        console.error(err)
+        alert(err)
+    }
+}
+
+document.getElementById('form').onsubmit = function (event) {
+    event.preventDefault()
+    draw()
+}
+
+draw()
+
 /* Changes the picture of the planet on the practice calculator */
 function changePlanet(parameter1) {
     if (parameter1 == "mercury") {
