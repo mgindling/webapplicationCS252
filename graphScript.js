60710a90818a65eb635ef9e5c9d8924f66649b2b
Mark Gindling <mgindlin@purdue.edu>
2019-04-21 20:39:33 -0400

    aaaaaaaaaa
    I was adding by cosine instead of multiplying by cosine. The graph works now.

--- graphScript.js
+++ graphScript.js
@@ -14,18 +14,18 @@ function draw() {
         // where h = 0 and v, a are set by the user (and pi is pi).
 
         // Constructs the equation.
-        var equation = "y = ((1/2)(" + currentgravity + ")) * (x / (" + currentpower + " + cos(" + currentangle + " * pi/180)))^2 + tan(" + currentangle + " * pi/180) * x";
+        var equation = "y = ((1/2)(" + currentgravity + ")) * (x / (" + currentpower + " * cos(" + currentangle + " * pi/180)))^2 + tan(" + currentangle + " * pi/180) * x";
 
         // Compiles the expression (the graph is drawn recursively, I believe)
         const compiledEquation = math.compile(equation);
 
         // Evaluates the expression repeatedly for different values of x. This is based on the distance calculated earlier
         var end; // Holds the end of the range of calculation (the beginning is always 0)
-        if (currentdistance > 500) {
+        if (currentdistance >= 499.75) {
             end = 500;            
         }
         else {
-            end = currentdistance;
+            end = currentdistance + .25;
         }
 
         // Sets the x-values for the functions and then maps the y-values.
